e1c623fd27dd1921aecf3efa61373d59c6bb3e59
Jelo <mark.jreyes107@gmail.com>
2025-07-08 21:33:44 +0800

    feat: added routes to verification

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,9 +1,17 @@
 import express from "express";
-import { register, login } from "../controllers/authController";
+
+import {
+  register,
+  login,
+  verifyCode,
+  resendCode,
+} from "../controllers/authController";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", register); // User signs up, gets 6-digit code
+router.post("/login", login); // Login allowed only if verified
+router.post("/verify-code", verifyCode); // POST { email, code }
+router.post("/resend-code", resendCode); // POST { email }
 
 export default router;
