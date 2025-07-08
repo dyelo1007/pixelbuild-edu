9273f92c9dacb06b5a0362352aa958b3a57403fd
Jelo <mark.jreyes107@gmail.com>
2025-07-08 21:38:34 +0800

    added wrapAsync utility to handle async errors in route controllers

---
+++ server/src/utils/wrapAsync.ts
@@ -0,0 +1,9 @@
+import { RequestHandler } from "express";
+
+const wrapAsync =
+  (fn: RequestHandler): RequestHandler =>
+  (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+
+export default wrapAsync;
