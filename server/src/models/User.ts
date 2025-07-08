ebea791232962cb6af5464b17c69d2db53cd2d87
Jelo <mark.jreyes107@gmail.com>
2025-07-08 21:32:04 +0800

    feat: add verificationCode and verificationCodeExpires fields to user schema

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,12 +1,29 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
 
-const userSchema = new mongoose.Schema({
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  role: string;
+  createdAt: Date;
+  token?: string;
+  isVerified: boolean;
+  verificationCode?: string;
+  verificationCodeExpires?: Date;
+}
+
+const userSchema = new mongoose.Schema<IUser>({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, default: "student" }, // for future admin
+  isVerified: { type: Boolean, default: false },
+
+  verificationCode: { type: String },
+  verificationCodeExpires: { type: Date },
+
   createdAt: { type: Date, default: Date.now },
   token: { type: String },
 });
 
-export const User = mongoose.model("User", userSchema);
+export const User = mongoose.model<IUser>("User", userSchema);
