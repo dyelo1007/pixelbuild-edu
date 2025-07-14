2b31c1cef427e2cbebfa4e76af8281a9f8c27147
Jelo <mark.jreyes107@gmail.com>
2025-07-14 13:56:55 +0800

    style(toast): apply custom styles and themes for toast notifications

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,12 +4,34 @@ import "./index.css";
 import App from "./App.tsx";
 import { AuthProvider } from "./auth/AuthContext";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <Toaster
+          toastOptions={{
+            className: "bg-darkbg text-white border border-primary",
+            style: {
+              background: "#1e1e1e", // fallback if your Tailwind class doesn't apply
+              color: "#fff",
+            },
+            success: {
+              iconTheme: {
+                primary: "#51ab91",
+                secondary: "#212121",
+              },
+            },
+            error: {
+              iconTheme: {
+                primary: "#f87171",
+                secondary: "#212121",
+              },
+            },
+          }}
+        />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
