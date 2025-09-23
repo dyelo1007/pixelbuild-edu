634032c55359900b14b00e8b2f81b98fd3ef3e50
Jelo <mark.jreyes107@gmail.com>
2025-09-23 16:17:26 +0800

    feat: added new controllers for admin

---
+++ server/src/controllers/adminController.ts
@@ -0,0 +1,106 @@
+import { Request, Response } from "express";
+import { User } from "../models/User";
+
+// Get all students
+export const getAllStudents = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const students = await User.find({ role: "student" }).select("-password");
+    res.json(students);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch students" });
+  }
+};
+
+//Get all users studs and admins
+
+export const getAllUsers = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const users = await User.find().select("username email role badges"); // include necessary fields
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch users" });
+  }
+};
+
+// Get student by ID
+export const getStudentById = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const student = await User.findById(req.params.id).select("-password");
+    if (!student) {
+      res.status(404).json({ message: "Student not found" });
+      return;
+    }
+    res.json(student);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch student" });
+  }
+};
+
+// pdate student info
+export const updateStudent = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const student = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    }).select("-password");
+
+    if (!student) {
+      res.status(404).json({ message: "Student not found" });
+      return;
+    }
+    res.json(student);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to update student" });
+  }
+};
+
+// Delete student
+export const deleteStudent = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const student = await User.findByIdAndDelete(req.params.id);
+    if (!student) {
+      res.status(404).json({ message: "Student not found" });
+      return;
+    }
+    res.json({ message: "Student deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete student" });
+  }
+};
+
+//mama mo promote
+export const promoteToAdmin = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const student = await User.findByIdAndUpdate(
+      req.params.id,
+      { role: "admin" },
+      { new: true }
+    ).select("-password");
+
+    if (!student) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    res.json({ message: "User promoted to admin", user: student });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to promote user" });
+  }
+};
