6c1931ce92ed13f2b574e072c4ed4537e9f2203a
Jelo <mark.jreyes107@gmail.com>
2025-10-05 21:33:42 +0800

    feat: added a logo

--- client/src/components/auth/AuthLayout.tsx
+++ client/src/components/auth/AuthLayout.tsx
@@ -1,3 +1,5 @@
+import Logo from "/pb-titlelogo.png";
+
 type AuthLayoutProps = {
   title: string;
   subtitle: string;
@@ -7,8 +9,8 @@ type AuthLayoutProps = {
 const AuthLayout = ({ title, subtitle, children }: AuthLayoutProps) => (
   <div className="min-h-screen flex justify-center items-center bg-lightbg dark:bg-darkbg rounded-2xl">
     <div className="border-3 border-neonblue rounded-xl px-10 py-8 w-full max-w-md backdrop-blur-md bg-lightbgfill dark:bg-darkbg/70 shadow-md">
-      <div className="flex justify-center mb-4">
-        <img src="/wala-pa-logo" alt="logo" className="w-10 h-10" />
+      <div className="flex justify-center  mb-6 items-center">
+        <img src={Logo} alt="logo" className="w-[100px] h-[40px]" />
       </div>
       <h2 className="dark:text-white text-neonblue text-2xl font-bold text-center">
         {title}
