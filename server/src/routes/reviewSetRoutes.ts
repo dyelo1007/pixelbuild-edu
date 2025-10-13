0672b3b7a50fae06d0873ceba3b0419189d6a097
Jelo <mark.jreyes107@gmail.com>
2025-10-13 22:07:39 +0800

    feat: added getreviewsets route

--- server/src/routes/reviewSetRoutes.ts
+++ server/src/routes/reviewSetRoutes.ts
@@ -6,6 +6,7 @@ import {
   getSetById,
   updateSet,
   deleteSet,
+  getMyReviewSets,
 } from "../controllers/reviewSetController";
 
 const router = Router();
@@ -13,6 +14,7 @@ const router = Router();
 router.use(protect);
 
 router.route("/").get(getMySets).post(createSet);
+router.get("/", protect, getMyReviewSets);
 
 router.route("/:id").get(getSetById).put(updateSet).delete(deleteSet);
 
